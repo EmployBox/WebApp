9cf2ade8fe1a47ae10fbcf1e313e3d90df2ae115
Diogo Rodrigues <dporodrigues@sapo.pt>
2018-02-25 21:59:15 +0000

    continuation of database creation script

--- SERVER/DB/DBScripts/SQL Server Scripts1/createDatabase.sql
+++ SERVER/DB/DBScripts/SQL Server Scripts1/createDatabase.sql
@@ -8,16 +8,33 @@ BEGIN
 END
 
 GO
-USE 'PS_API_DATABASE'
+USE PS_API_DATABASE
 GO
 
 
 CREATE TABLE ApiDatabase.[Account] (
-	account_id INT IDENTITY PRIMARY KEY,
-	email TEXT UNIQUE NOT NULL,
-	rating decimal(2,1) CONSTRAINT rate_const default(0.0),
-	password_hash NVARCHAR(50) NOT NULL,
-	salt UNIQUEIDENTIFIER NOT NULL
+	accountId BIGINT IDENTITY PRIMARY KEY,
+	email NVARCHAR UNIQUE NOT NULL,
+	rating decimal(2,1) default(0.0),
+	passwordHash NVARCHAR(50) NOT NULL,
+	salt UNIQUEIDENTIFIER NOT NULL,
 
 	CONSTRAINT rate_const CHECK (rating >= 0.0 AND rating <= 5.0)
+)
+
+CREATE TABLE ApiDatabase.Company (
+	accountId BIGINT IDENTITY primary key references ApiDatabase.[Account],
+	name NVARCHAR(40),
+	yearFounded SMALLINT,
+	Specialization NVARCHAR(20),
+	WebPageUrl NVARCHAR(50),
+	LogoUrl NVARCHAR(100),
+	description NVARCHAR(50)
+)
+
+CREATE TABLE ApiDatabase.SiteUser (
+	accountId BIGINT IDENTITY primary key references ApiDatabase.[Account],
+	name NVARCHAR(40),
+	summary NVARCHAR(1500),
+	PhotoUrl  NVARCHAR(100)
 )
--- SERVER/WEB_API/src/main/java/dataMapper/AccountMapper.java
+++ SERVER/WEB_API/src/main/java/dataMapper/AccountMapper.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentMap;
 
