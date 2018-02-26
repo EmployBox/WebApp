0fb9c115a5e8964c78b83bc0a5488769e5fa27ee
Diogo Rodrigues <dporodrigues@sapo.pt>
2018-02-26 22:54:04 +0000

    added view to the database to simplify query operations that requires addition of collumns and to hide sensitive data

--- SERVER/DB/DBScripts/SQL Server Scripts1/createDatabase.sql
+++ SERVER/DB/DBScripts/SQL Server Scripts1/createDatabase.sql
@@ -186,3 +186,68 @@ CREATE TABLE ApiDatabase.Follows (
 
 	primary key (AccountIdFrom,AccountIdDest)
 )
+
+GO
+CREATE VIEW dbo.Account AS
+SELECT accountId,email,rating FROM ApiDatabase.Account
+GO
+
+GO
+CREATE VIEW dbo.[User] AS
+SELECT * FROM ApiDatabase.[User]
+GO
+
+GO
+CREATE VIEW dbo.Company AS
+SELECT * FROM ApiDatabase.Company
+GO
+
+GO
+CREATE VIEW dbo.Job AS
+SELECT * FROM ApiDatabase.Job
+GO
+
+GO
+CREATE VIEW dbo.Curriculum AS
+SELECT * FROM ApiDatabase.Curriculum
+GO
+
+GO
+CREATE VIEW dbo.Experience AS
+SELECT * FROM ApiDatabase.Experience
+GO
+
+GO
+CREATE VIEW dbo.Curriculum_Experience AS
+SELECT * FROM ApiDatabase.Curriculum_Experience
+GO
+
+GO
+CREATE VIEW dbo.Job_Experience AS
+SELECT * FROM ApiDatabase.Job_Experience
+GO
+
+GO
+CREATE VIEW dbo.[Local] AS
+SELECT * FROM ApiDatabase.[Local]
+GO
+
+GO
+CREATE VIEW dbo.Comment AS
+SELECT * FROM ApiDatabase.Comment
+GO
+
+GO
+CREATE VIEW dbo.Chat AS
+SELECT * FROM ApiDatabase.Chat
+GO
+
+GO
+CREATE VIEW dbo.[Message] AS
+SELECT * FROM ApiDatabase.[Message]
+GO
+
+GO
+CREATE VIEW dbo.Follows AS
+SELECT * FROM ApiDatabase.Follows
+GO
