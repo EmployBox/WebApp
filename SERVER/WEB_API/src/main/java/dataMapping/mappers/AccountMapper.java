fcd4591015d79d5efd7c463e19c4eb3b3e085a0c
Diogo Rodrigues <dporodrigues@sapo.pt>
2018-02-28 19:53:11 +0000

    Implementation UserMapper

--- SERVER/WEB_API/src/main/java/dataMapping/mappers/AccountMapper.java
+++ SERVER/WEB_API/src/main/java/dataMapping/mappers/AccountMapper.java
@@ -1,151 +1,24 @@
 package dataMapping.mappers;
 
-import dataMapping.exceptions.DataMapperException;
-import dataMapping.utils.ConnectionManager;
 import model.Account;
 
-import java.sql.*;
-import java.util.Optional;
-
-public class AccountMapper extends AbstractMapper<Account> {
-    private final String SELECT_QUERY = "SELECT AccountID, Email, passwordHash, Rating FROM Account WHERE Email = ?";
+public abstract class AccountMapper<T extends Account> extends AbstractMapper<T> {
+    private final String SELECT_QUERY = "SELECT AccountID, Email, passwordHash, Rating FROM Account WHERE AccountID = ?";
     private final String INSERT_QUERY = "INSERT INTO Account (Email, Password, Rating, Version) VALUES (?, ?, ?, ?)";
     private final String UPDATE_QUERY = "UPDATE Account SET Password = ?, Rating = ?, Version = ? WHERE AccountID = ? AND Version = ?";
     private final String DELETE_QUERY = "DELETE FROM Account WHERE AccountID = ? AND Version = ?";
 
-    public Optional<Account> findByEmail(String email) throws DataMapperException {
-        return findByPrimaryKey(email);
-    }
 
     @Override
     protected String findByPKStatement() {
         return SELECT_QUERY;
     }
 
-    @Override
-    public Account mapper(ResultSet rs) throws DataMapperException {
-        try {
-            long accountID = rs.getLong("AccountID");
-            String email = rs.getString("Email");
-            String password = rs.getString("PasswordHash");
-            Double rate = rs.getDouble("Rating");
 
-            Account account = Account.load(accountID, email, password, rate, 0);
-            getIdentityMap().put(email, account);
+    public abstract void insert(T obj);
 
-            return account;
-        } catch (SQLException e) {
-            throw new DataMapperException(e.getMessage(), e);
-        }
-    }
-
-//    private Account load(ResultSet rs) throws DataMapperException {
-//        try {
-//                String email = rs.getString("Email");
-//                String password = rs.getString("Password");
-//                Double rate = rs.getDouble("Rate");
-//
-//                Account account = Account.load(email, password, rate);
-//                getIdentityMap().put(email, account);
-//
-//                return account;
-//        } catch (SQLException e) {
-//            throw new DataMapperException(e.getMessage(), e);
-//        }
-//    }
-
-    @Override
-    public void insert(Account obj) {
-        /*DBHelper(
-                INSERT_QUERY,
-                stmt -> {
-                    SQLException sqlException = null;
-                    try {
-                        stmt.setString(1, obj.getEmail());
-                        stmt.setString(2, obj.getPassword());
-                        stmt.setDouble(3, obj.getRating());
-                        stmt.setLong(4, obj.getVersion());
-                    } catch (SQLException e) {
-                       sqlException = e;
-                    }
-                    return sqlException;
-                },
-                stmt -> {
-                    SQLException sqlException = null;
-                    try {
-                        ResultSet rs = stmt.getGeneratedKeys();
-                        rs.next();
-                        Account account = Account.load(rs.getLong(1), obj.getEmail(), obj.getPassword(), obj.getRating(), obj.getVersion());
-                        getIdentityMap().put(account.getIdentityKey(), account);
-                    } catch (SQLException e) {
-                        sqlException = e;
-                    }
-                    return sqlException;
-                }
-        );*/
-
-        /*cs = this.con.prepareCall("{call GET_SUPPLIER_OF_COFFEE(?, ?)}");
-        cs.setString(1, coffeeNameArg);
-        cs.registerOutParameter(2, Types.VARCHAR);
-        cs.executeQuery();
-
-        String supplierName = cs.getString(2);*/
+    public abstract void update(T obj);
 
 
-        Connection con = ConnectionManager.getConnectionManager().getConnection();
-        try{
-            CallableStatement cs = con.prepareCall("{call AddAccount(?, ?, ?, ?, ?)}");
-            cs.setString(1, obj.getEmail());
-            cs.setDouble(2, obj.getRating());
-            cs.setString(3, obj.getPassword());
-            cs.registerOutParameter(4, Types.BIGINT);
-            cs.registerOutParameter(5, Types.NVARCHAR);
-            cs.execute();
-            long accountId = cs.getLong(4);
-
-            Account account = Account.load(accountId, obj.getEmail(), obj.getPassword(), obj.getRating(), obj.getVersion());
-            getIdentityMap().put(account.getIdentityKey(), account);
-        } catch (SQLException e) {
-            throw new DataMapperException(e.getMessage(), e);
-        }
-    }
-
-    @Override
-    public void update(Account obj) {
-        DBHelper(
-                UPDATE_QUERY,
-                stmt -> {
-                    SQLException sqlException = null;
-                    try{
-                        stmt.setString(1, obj.getPassword());
-                        stmt.setDouble(2, obj.getRating());
-                        stmt.setLong(3, obj.getVersion());
-                        stmt.setLong(4, obj.getAccountID());
-                        stmt.setLong(5, obj.getVersion()-1);
-                    }catch (SQLException e){
-                        sqlException = e;
-                    }
-                    return sqlException;
-                },
-                () -> getIdentityMap().put(obj.getIdentityKey(), obj)
-        );
-    }
-
-    @Override
-    public void delete(Account obj) {
-        DBHelper(
-                DELETE_QUERY,
-                stmt -> {
-                    SQLException sqlException = null;
-                    try{
-                        stmt.setLong(1, obj.getAccountID());
-                        stmt.setLong(2, obj.getVersion());
-                    } catch (SQLException e) {
-                        sqlException = e;
-                    }
-                    return sqlException;
-                },
-                () -> getIdentityMap().remove(obj.getIdentityKey())
-        );
-    }
+    public abstract void delete(T obj);
 }
--- SERVER/WEB_API/src/main/java/dataMapping/mappers/UserMapper.java
+++ SERVER/WEB_API/src/main/java/dataMapping/mappers/UserMapper.java
@@ -2,41 +2,86 @@ package dataMapping.mappers;
 
 
 import dataMapping.exceptions.DataMapperException;
-import model.DomainObject;
+import dataMapping.utils.ConnectionManager;
 import model.User;
 
-import java.sql.ResultSet;
+import java.sql.*;
 
 //todo i must finish this
-public class UserMapper extends AbstractMapper {
+public class UserMapper extends AccountMapper<User> {
     private final String SELECT_QUERY = "SELECT AccountId, Name, Summary, PhotoUrl FROM User WHERE AccountId = ?";
-
-    private final String UPDATE_QUERY = "UPDATE Job SET Wage = ?, Description = ?, Schedule = ?, OfferBeginDate = ?, OfferEndDate = ?, OfferType = ?, Version = ? WHERE JobID = ? AND Version = ?";
-    private final String DELETE_QUERY = "DELETE FROM Job WHERE JobID = ? AND Version = ?";
-
 
     @Override
     protected String findByPKStatement() {
         return SELECT_QUERY;
     }
 
+
     @Override
-    User mapper(ResultSet rs) throws DataMapperException {
-        return null;
+    public User mapper(ResultSet rs) throws DataMapperException {
+        try {
+            long accountID = rs.getLong("AccountID");
+            String email = rs.getString("Email");
+            String passwordHash = rs.getString("PasswordHash");
+            Double rating = rs.getDouble("Rating");
+            String name = rs.getString ("Name");
+            String summary = rs.getString ("Summary");
+            String photoUrl = rs.getString ("PhotoUrl");
+
+            User user = User.load(accountID, email, passwordHash, rating, 0,name ,summary, photoUrl );
+            getIdentityMap().put(email, user);
+
+            return user;
+        } catch (SQLException e) {
+            throw new DataMapperException(e.getMessage(), e);
+        }
     }
 
     @Override
-    public void insert(DomainObject obj) {
-
+    public void insert(User obj) {
+        writeUser(obj,"{call AddUser(?, ?, ?, ?, ?, ?, ?, ?)}");
     }
 
     @Override
-    public void update(DomainObject obj) {
+    public void update(User obj) {
+        writeUser(obj,"{call UpdateUser(?, ?, ?, ?, ?, ?, ?, ?)}");
+    }
 
+    private  void writeUser(User obj,String procedureToUse){
+        Connection con = ConnectionManager.getConnectionManager().getConnection();
+        try{
+            CallableStatement cs = con.prepareCall(procedureToUse);
+            cs.setString(1, obj.getEmail());
+            cs.setDouble(2, obj.getRating());
+            cs.setString(3, obj.getPassword());
+            cs.setString( 4, obj.getName());
+            cs.setString(5, obj.getSummary());
+            cs.setString(6, obj.getPhotoUrl());
+            cs.registerOutParameter(7, Types.BIGINT);
+            cs.registerOutParameter(8, Types.NVARCHAR);
+
+            cs.execute();
+
+            getIdentityMap().put(obj.getIdentityKey(), obj);
+        } catch (SQLException e) {
+            throw new DataMapperException(e.getMessage(), e);
+        }
     }
 
     @Override
-    public void delete(DomainObject obj) {
+    public void delete(User obj) {
+        Connection con = ConnectionManager.getConnectionManager().getConnection();
+        try{
+            CallableStatement cs = con.prepareCall("{call DeleteUser(?, ?)}");
+            cs.setString(1, obj.getEmail());
+            cs.registerOutParameter(2, Types.NVARCHAR);
 
+            cs.execute();
+
+            getIdentityMap().remove(obj.getIdentityKey());
+
+        } catch (SQLException e) {
+            throw new DataMapperException(e.getMessage(), e);
+        }
     }
 }
