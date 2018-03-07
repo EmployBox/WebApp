83a1072066c9d714cef7e6faaf7cbd8e9d20a4ff
Joao360 <41423@alunos.isel.ipl.pt>
2018-03-07 20:08:08 +0000

    Adds Follow and FollowMapper, finishes MessageMapper, RatingMapper, ChatMapper

--- SERVER/WEB_API/src/main/java/dataMapping/mappers/ChatMapper.java
+++ SERVER/WEB_API/src/main/java/dataMapping/mappers/ChatMapper.java
@@ -2,29 +2,67 @@ package dataMapping.mappers;
 
 import dataMapping.exceptions.DataMapperException;
 import dataMapping.utils.MapperSettings;
+import javafx.util.Pair;
 import model.Chat;
 import model.DomainObject;
+import model.Message;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import util.Streamable;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class ChatMapper extends AbstractMapper{
-    public ChatMapper(MapperSettings insertSettings, MapperSettings updateSettings, MapperSettings deleteSettings) {
-        super(insertSettings, updateSettings, deleteSettings);
+import static dataMapping.utils.MapperRegistry.getMapper;
+
+public class ChatMapper extends AbstractMapper<Chat, Long>{
+    private static final String SELECT_QUERY = "SELECT chatId, AccountIdFirst, AccountIdSecond, [version] FROM Chat";
+    private static final String INSERT_QUERY = "INSERT INTO Chat (chatId, AccountIdFirst, AccountIdSecond) VALUES (?, ?, ?)";
+    private static final String DELETE_QUERY = "DELETE FROM Chat WHERE chatId = ? AND AccountIdFirst = ? AND AccountIdSecond = ?";
+
+    public ChatMapper() {
+        super(
+                new MapperSettings<>(INSERT_QUERY, PreparedStatement.class, ChatMapper::prepareStatement),
+                null,
+                new MapperSettings<>(DELETE_QUERY, PreparedStatement.class, ChatMapper::prepareStatement)
+        );
+    }
+
+    public Streamable<Chat> findForAccount(long accountID) {
+        return findWhere(new Pair<>("accountIdFirst", accountID));
     }
 
     @Override
-    DomainObject mapper(ResultSet rs) throws DataMapperException {
-        return null;
+    Chat mapper(ResultSet rs) throws DataMapperException {
+        try{
+            long chatId = rs.getLong(1);
+            long accountIdFirst = rs.getLong(2);
+            long accountIdSecond = rs.getLong(3);
+            long version = rs.getLong(4);
+
+            Streamable<Message> messages = ((MessageMapper) getMapper(Message.class)).findForChat(chatId);
+
+            Chat chat = Chat.load(chatId, accountIdFirst, accountIdSecond, version, messages);
+            identityMap.put(chat.getIdentityKey(), chat);
+
+            return chat;
+        } catch (SQLException e) {
+            throw new DataMapperException(e);
+        }
     }
 
     @Override
     String getSelectQuery() {
-        return null;
+        return SELECT_QUERY;
     }
 
-    public Streamable<Chat> findChatsForAccount(long accountID) {
-        throw new NotImplementedException();
+    private static void prepareStatement(PreparedStatement statement, Chat obj){
+        try{
+            statement.setLong(1, obj.getChadId());
+            statement.setLong(2, obj.getAccountIdFirst());
+            statement.setLong(3, obj.getAccountIdSecond());
+        } catch (SQLException e) {
+            throw new DataMapperException(e);
+        }
     }
 }
