779ef12a2ffd69f3d16a4838430bbded62162e92
Joao360 <41423@alunos.isel.ipl.pt>
2018-04-02 18:35:30 +0100

    Deletes DataBaseTests and adds JobExperienceBinder

--- SERVER/WEB_API/src/main/java/isel/ps/employbox/api/model/binder/JobBinder.java
+++ SERVER/WEB_API/src/main/java/isel/ps/employbox/api/model/binder/JobBinder.java
@@ -4,6 +4,7 @@ import isel.ps.employbox.api.model.input.InJob;
 import isel.ps.employbox.api.model.output.OutJob;
 import isel.ps.employbox.api.model.ModelBinder;
 import isel.ps.employbox.dal.model.Job;
+import isel.ps.employbox.dal.model.JobExperience;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,9 +12,9 @@ import java.util.stream.Collectors;
 
 @Component
 public class JobBinder implements ModelBinder<Job, OutJob, InJob, Long> {
-    private final ExperienceBinder experienceBinder;
+    private final ModelBinder<JobExperience, OutJob.OutExperience, InJob.InExperience, Long> experienceBinder;
 
-    public JobBinder(ExperienceBinder experienceBinder){
+    public JobBinder(ModelBinder<JobExperience, OutJob.OutExperience, InJob.InExperience, Long> experienceBinder){
         this.experienceBinder = experienceBinder;
     }
 
@@ -21,61 +22,32 @@ public class JobBinder implements ModelBinder<Job, OutJob, InJob, Long> {
     public List<OutJob> bindOutput(List<Job> list) {
         return list
             .stream()
-            .map((curr)-> new OutJob(
-                curr.getAccountID(),
-                    title, experienceBinder.bindOutput(curr.getExperiences().get()),
-                curr.getAddress(),
-                curr.getWage(),
-                curr.getDescription(),
-                curr.getSchedule(),
-                curr.getOfferBeginDate(),
-                curr.getOfferEndDate(),
-                curr.getOfferType()))
+            .map((curr)-> new OutJob(curr.getAccountID(), curr.getTitle(), experienceBinder.bindOutput(curr.getExperiences().get()), curr.getAddress(), curr.getWage(),
+                    curr.getDescription(), curr.getSchedule(), curr.getOfferBeginDate(), curr.getOfferEndDate(), curr.getOfferType()))
             .collect(Collectors.toList());
     }
 
     @Override
     public List<Job> bindInput(List<InJob> list) {
         return list
-            .stream()
-            .map((curr)-> new Job(
-                curr.getAccountID(),
-                curr.getJobID(),
-                    title, curr.getAddress(),
-                curr.getWage(),
-                curr.getDescription(),
-                curr.getSchedule(),
-                curr.getOfferBeginDate(),
-                curr.getOfferEndDate(),
-                curr.getOfferType()))
-            .collect(Collectors.toList());
+                .stream()
+                .map((curr)-> new Job(curr.getAccountID(), curr.getJobID(), curr.getTitle(), curr.getAddress(), curr.getWage(), curr.getDescription(), curr.getSchedule(),
+                        curr.getOfferBeginDate(), curr.getOfferEndDate(), curr.getOfferType())
+                )
+                .collect(Collectors.toList());
     }
 
     @Override
     public OutJob bindOutput(Job job) {
-        return new OutJob(
-            job.getAccountID(),
-                title, experienceBinder.bindOutput(job.getExperiences().get()),
-            job.getAddress(),
-            job.getWage(),
-            job.getDescription(),
-            job.getSchedule(),
-            job.getOfferBeginDate(),
-            job.getOfferEndDate(),
-            job.getOfferType());
+        return new OutJob(job.getAccountID(), job.getTitle(), experienceBinder.bindOutput(job.getExperiences().get()), job.getAddress(), job.getWage(), job.getDescription(),
+                job.getSchedule(), job.getOfferBeginDate(), job.getOfferEndDate(), job.getOfferType()
+        );
     }
 
     @Override
     public Job bindInput(InJob curr) {
-        return new Job(
-            curr.getAccountID(),
-            curr.getJobID(),
-                title, curr.getAddress(),
-            curr.getWage(),
-            curr.getDescription(),
-            curr.getSchedule(),
-            curr.getOfferBeginDate(),
-            curr.getOfferEndDate(),
-            curr.getOfferType());
+        return new Job(curr.getAccountID(), curr.getJobID(), curr.getTitle(), curr.getAddress(), curr.getWage(), curr.getDescription(), curr.getSchedule(),
+                curr.getOfferBeginDate(), curr.getOfferEndDate(), curr.getOfferType()
+        );
     }
 }
