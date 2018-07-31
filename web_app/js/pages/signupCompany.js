6866e14ced3aaa3583a42281e388029bd23a3c33
tdx96 <A41557@alunos.isel.pt>
2018-07-31 16:48:00 +0100

    sign up completed

--- web_app/js/pages/signupCompany.js
+++ web_app/js/pages/signupCompany.js
@@ -8,7 +8,12 @@ export default ({url}) => (
       inputData={[
         {type: 'text', name: 'name', label: 'Name', id: 'usernameID'},
         {type: 'text', name: 'email', label: 'Email address', id: 'emailID'},
-        {type: 'password', name: 'password', label: 'Password', id: 'passID'}
+        {type: 'password', name: 'password', label: 'Password', id: 'passID'},
+        {type: 'text', name: 'specialization', label: 'Specialization', id: 'specializationID'},
+        {type: 'number', name: 'yearFounded', label: 'Year Founded', id: 'yearFoundedID'},
+        {type: 'url', name: 'logoUrl', label: 'Logo URL', id: 'logoUrlID'},
+        {type: 'url', name: 'webpageUrl', label: 'WebPage URL', id: 'webpageUrlID'},
+        {type: 'text', name: 'description', label: 'Description', id: 'descriptionID'}
       ]}
       klass='form-group'
       onSubmitHandler={cb}
--- web_app/js/pages/signupUser.js
+++ web_app/js/pages/signupUser.js
@@ -8,7 +8,9 @@ export default ({url}) => (
       inputData={[
         {type: 'text', name: 'name', label: 'Name', id: 'usernameID'},
         {type: 'text', name: 'email', label: 'Email address', id: 'emailID'},
-        {type: 'password', name: 'password', label: 'Password', id: 'passID'}
+        {type: 'password', name: 'password', label: 'Password', id: 'passID'},
+        {type: 'url', name: 'photoURL', label: 'Photo URL', id: 'photoUrlID'},
+        {type: 'text', name: 'summary', label: 'Summary', id: 'summaryID'}
       ]}
       klass='form-group'
       onSubmitHandler={cb}
